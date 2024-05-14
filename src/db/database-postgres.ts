bb950990aa14b379a16472f18a816ceddf10e485
viniibp <viniibp@gmail.com>
2024-05-14 14:07:26 -0300

    feat: melhorias na sintaxe do sql com base na doc do postgres

--- src/db/database-postgres.ts
+++ src/db/database-postgres.ts
@@ -1,12 +1,14 @@
 import { randomUUID } from "node:crypto";
 import type { IVideo } from "../interface";
 import { sql } from "./database";
-
 export class DatabasePostgres {
   async list(search?: string) {
     let videos;
+
     if (search) {
-      videos = await sql`select * from videos where title ilike ${'%'+search+'%'};`;
+      videos = await sql`select * from videos where title ilike ${
+        "%" + search + "%"
+      };`;
     } else {
       videos = await sql`select * from videos;`;
     }
@@ -15,21 +17,15 @@ export class DatabasePostgres {
 
   async create(video: IVideo) {
     const videoId = randomUUID();
-    const { title, description, duration } = video;
 
-    await sql`
-    INSERT INTO videos (id, title, description, duration) 
-    VALUES (${videoId}, ${title}, ${description}, ${duration});`;
+    await sql`INSERT INTO videos ${sql({ id: videoId, ...video })};`;
   }
 
   async update(id: string, video: IVideo) {
-    const { title, description, duration } = video;
-
-    await sql`
-    UPDATE videos set title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id};`;
+    await sql`UPDATE videos set ${sql(video)} WHERE id = ${id};`;
   }
 
   async delete(id: string) {
-    await sql`DELETE FROM videos WHERE id = ${id};`
+    await sql`DELETE FROM videos WHERE id = ${id};`;
   }
 }
